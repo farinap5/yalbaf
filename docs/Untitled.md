6bde579fe49d1970344408797839a838a3215564
farinap5 <farinap5@protonmail.com>
2024-10-23 11:50:48 -0300

    add: x-forwarded-for header

---
+++ docs/Untitled.md
@@ -0,0 +1,7 @@
+```sql
+SELECT 1 FROM X WHERE v='%s';
+
+SELECT 1 FROM X WHERE v='union select 1 from x where 1 = 1';
+
+SELECT 1 FROM X WHERE v='batata'';
+```
--- pkg/server/handler.go
+++ pkg/server/handler.go
@@ -29,21 +29,16 @@ func (s Server)analyzer(prx http.HandlerFunc) http.HandlerFunc {
 		}
 		if attack {
 			w.WriteHeader(403)
-			w.Write([]byte("Attack detected!"))
+			w.Write([]byte("Malicious request detected!"))
 			return
 		}
-		/* 
-			Implement everything here
 
-			
-			io.Copy(w, bloq-page)
-		*/
 		prx(w, r)
 	}
 }
 
 /*
-	Passthrough proxy. Mirror the interation of the client with the upstream.
+	Passthrough proxy. Mirror the interaction of the client with the upstream.
 	The body must update in chunks.
 */
 func (s Server)proxy(target string) http.HandlerFunc {
@@ -61,6 +56,9 @@ func (s Server)proxy(target string) http.HandlerFunc {
 		client := &http.Client{
 			Timeout: 5 * time.Second,
 		}
+
+		CompileXForwardHead(req.Header, r.RemoteAddr)
+		
 		resp, err := client.Do(req)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadGateway)
@@ -70,7 +68,7 @@ func (s Server)proxy(target string) http.HandlerFunc {
 
 		MirrorHeader(w.Header(), resp.Header)
 		w.WriteHeader(resp.StatusCode)
-		
+
 		_, err = io.Copy(w, resp.Body)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadGateway)
